17731d76ff40835a57f09bf422ddf7b7250d8feb
unknown <wowns9270@gmail.com>
2021-01-16 15:58:41 +0900

    inital

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
+const handlebars = require("handlebars");
+const axios = require('axios');
+
+const helmet = require('helmet');
+const hpp = require('hpp');
+
+
+let indexRouter = require('./routes/index');
+let app = express(); // 연결?
+
+app.set( 'port' , process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.enable('trust proxy');
+    app.use(morgan('combined'));
+    app.use(helmet({contentSecurityPolicy : false}));
+    app.use(hpp());
+}
+else{
+    app.use(morgan('dev'));
+}
+
+
+app.engine(
+    "hbs",
+    hbs({
+      extname: "hbs",
+      defaultLayout: "layout", // 기본레이아웃 설정
+      // 레이아웃 디렉토리 설정.. 리액트에서 레이아웃 따로 정하면 필요없단다?...
+      layoutsDir: __dirname + "/views/layouts",
+      // 반복적인 html코드가 있다면 아래 지정경로에서 가져다 쓸수 있다.
+      partialsDir: __dirname + "/view/partials"
+    })
+  );
+  app.set('view engine','hbs') //
+
+const mainRouter = require('./routes/index');
+
+app.use('/' , mainRouter);
+
+
+app.listen( app.get('port') , ()=>{
+    console.log(`성공 ${app.get('port')}`);
+} )
---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+require('dotenv').config();
+
+
+const router = express.Router();
+
+
+router.get('/' , (req,res) =>{
+    res.render('main');
+});
+
+
+router.post('/' , async(req , res) =>{
+    try{
+
+        const newId = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURI(req.body.id)}?api_key=${process.env.api_key}`);
+        
+        //console.log(newId);
+        
+        const info = await axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${newId.data.id}?api_key=${process.env.api_key}`);
+
+        //console.log(info.data[0]);
+
+        if(info.data[0] === undefined){
+            res.render('not_user');
+        }
+        else{
+    //console.log(info.data);
+             res.render('user' , {
+                user : info.data[0],
+            })
+        }   
+    
+    }catch(e){
+        console.log(e);
+    }
+});
+
+
+
+module.exports = router;
